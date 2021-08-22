66e0e19542e98558bf4588a329d215cc0593c891
Albus Piroglu <mattapiroglu@gmail.com>
2021-08-22 16:55:11 +1000

    sbcl layout-info -> wrapper-info etc.

--- inspect.lisp
+++ inspect.lisp
@@ -762,7 +762,7 @@ cons cells and LIST-TYPE is :normal, :dotted, or :cyclic"
 
 (defun inspected-structure-parts (object)
   (let ((components-list '())
-        #+sbcl (info (sb-kernel:layout-info (sb-kernel:layout-of object))))
+        #+sbcl (info (sb-kernel:wrapper-info (sb-kernel:wrapper-of object))))
     #+sbcl
     (when (sb-kernel::defstruct-description-p info)
       (dolist (dd-slot (sb-kernel:dd-slots info))
